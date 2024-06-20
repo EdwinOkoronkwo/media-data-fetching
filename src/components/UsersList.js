13ae688e330b807cb20115cbe4cbfe064f2357be
Edwin Okoronkwo <49468467+EdwinOkoronkwo@users.noreply.github.com>
2024-06-20 17:44:21 -0600

    Added Skeleton Loaded using TailwindCSS

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,12 +1,24 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers } from "../store";
+import Skeleton from "./Skeleton";
 
 export default function UsersList() {
   const dispatch = useDispatch();
+  const { isLoading, data, error } = useSelector((state) => {
+    return state.users;
+  });
 
   useEffect(() => {
     dispatch(fetchUsers());
   }, [dispatch]);
-  return <div>UsersList</div>;
+
+  if (isLoading) {
+    return <Skeleton times={6} className="h-10 w-full" />;
+  }
+
+  if (error) {
+    return <div>Error fetching data...</div>;
+  }
+  return <div>{data.length}</div>;
 }
