1b912990b0faf25e6cb09955a4240cabc34ffbaf
Edwin Okoronkwo <49468467+EdwinOkoronkwo@users.noreply.github.com>
2024-06-21 20:09:48 -0600

    End of Project

---
+++ src/components/AlbumsListItem.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Button from "./Button";
+import ExpandablePanel from "./ExpandablePanel";
+import { GoTrashcan } from "react-icons/go";
+import { useRemoveAlbumMutation } from "../store";
+import PhotosList from "./PhotosList";
+
+export default function AlbumsListItem({ album }) {
+  const [removeAlbum, results] = useRemoveAlbumMutation();
+
+  const handleRemoveAlbum = () => {
+    removeAlbum(album);
+  };
+  const header = (
+    <>
+      <Button
+        onClick={handleRemoveAlbum}
+        loading={results.isLoading}
+        className="mr-2"
+      >
+        <GoTrashcan />
+      </Button>
+      {album.title}
+    </>
+  );
+  return (
+    <ExpandablePanel key={album.id} header={header}>
+      <PhotosList album={album} />
+    </ExpandablePanel>
+  );
+}
